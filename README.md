0184297738fe30445b168ae2f09e3449bd45237e
Amuuuuuuuu <oupquv@gmail.com>
2026-05-10 00:46:21 +0800

    :feat: update params

---
+++ README.md
@@ -0,0 +1,6 @@
+
+```
+trunk build --release --public-url https://nikkaku.github.io/leptos-practise/
+trunk serve --open
+trunk serve
+```
--- src/app.rs
+++ src/app.rs
@@ -1,14 +1,48 @@
+use leptos::prelude::*;
+use leptos_router::{
+  components::{Router, Routes, Route},
+  hooks::{ use_params },
+  params::Params,
+  path
+};
+
 use crate::components::header::Header;
 use crate::components::footer::Footer;
-use leptos::prelude::*;
 
 #[component]
 pub fn App() -> impl IntoView {
   view! {
-    <Header />
-    <main>
-      "main"
-    </main>
-    <Footer />
+    <Router>
+      <Header />
+      <main>
+        <Routes fallback=|| "Not found.">
+          <Route path=path!("/") view=Home/>
+          <Route path=path!("/post/:id") view=BlogPost/>
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
   }
+}
+
+#[component]
+fn Home() -> impl IntoView { view! { <h1>"home"</h1> } }
+
+#[derive(Params, PartialEq)]
+struct ContactParams {
+  id: Option<usize>
+}
+
+#[component]
+fn BlogPost() -> impl IntoView {
+  let params = use_params::<ContactParams>();
+  let id = move || {
+    params
+        .read()
+        .as_ref()
+        .ok()
+        .and_then(|params| params.id)
+        .unwrap_or_default()
+  };
+  view! { <article>"article id:"{id}</article> }
 }
